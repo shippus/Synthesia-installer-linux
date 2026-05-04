be80098349fcda9aba57955a44ba4abc1bd64daf
shippuss <shippus_me@proton.me>
2026-05-04 15:09:45 -0600

    Fixed several bugs and the repo's path isn't hard coded anymore.

--- install.sh
+++ install.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 # Get the synthesia installer.
-curl -O synthesia_installer.exe https://www.synthesiagame.com/get?platform=win
+curl -o synthesia_installer.exe https://www.synthesiagame.com/get?platform=win
 wine synthesia_installer.exe
 read -p "Press Enter once the installation has succeeded"
 
-# Once Synthesia is installed, I go to the installation
-cd $HOME/.wine/drive_c/Program Files (x86)/Synthesia/
-wine $HOME/.wine/drive_c/Program Files (x86)/Synthesia/Synthesia.exe
-
-# Opens Synthesia and meanwhile I create the .desktop
+# Create the .desktop
 mkdir -p $HOME/.local/bin/
 export PATH="$HOME/.local/bin:$PATH"
-cat << EOF > $HOME/.local/bin/synthesia
+cat <<EOF >$HOME/.local/bin/synthesia
 #!/bin/bash
-
 zenity --info --text="Opening Synthesia with Wine."
 
 # Opens Synthesia
@@ -22,18 +17,22 @@ cd "$HOME/.wine/drive_c/Program Files (x86)/Synthesia/"
 exec wine "$HOME/.wine/drive_c/Program Files (x86)/Synthesia/Synthesia.exe" "$@"
 EOF
 
-cp $HOME/synthesia-installer-linux/synthesia.png $HOME/.local/share
+chmod +x $HOME/.local/bin/synthesia
 
-mkdir -p ~/.local/share/applications/
-cat << EOF > $HOME/.local/share/applications/
+mkdir -p ~/.local/share/icons/
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cp "$SCRIPT_DIR/synthesia.png" "$HOME/.local/share/icons/synthesia.png"
+mkdir -p ~/.local/share/applications/synthesia.desktop
+cat <<EOF >$HOME/.local/share/applications/
 [Desktop Entry]
 Name=Synthesia
 Comment=Piano learning game
 Exec=synthesia
-Icon=$HOME/.local/share/synthesia.png
+Icon=$HOME/.local/share/icons/synthesia.png
 Type=Application
 Categories=Game;Music;Education;
 StartupWMClass=synthesia.exe
 Terminal=false
 EOF
 
+update-desktop-database ~/.local/share/applications/
